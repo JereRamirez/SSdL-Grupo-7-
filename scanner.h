c6fca9c7f50d6f1a117bcc34c38f454275de1ef3
JhoseAruquipa <37785382+Jhosearuquipa@users.noreply.github.com>
2018-06-06 01:09:03 -0300

    Update scanner.h

--- scanner.h
+++ scanner.h
@@ -1,6 +1,13 @@
 #ifndef SCANNER_H_INCLUDED
 #define SCANNER_H_INCLUDED
-
-char scanner(FILE *automata);
-
+/*funciones*/
+enum token{T_LETRA, T_NUMERO,T_ESPACIOS,T_EOF,T_ERROR};
+char scanner(FILE *automata); //funcion principal Automata Finito Deterministico
+int debo_parar(int estado);
+int esLetra(char l);
+int esNumero(char l);
+int esEspacio(char l);
+int obtener_columna (char caracter);
+/*funciones*/
 #endif // SCANNER_H_INCLUDED
+
